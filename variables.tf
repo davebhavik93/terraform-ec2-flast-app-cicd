e723a53207a67d5e4b7fbaaad452238eba48b4fb
davebhavik93 <davebhavik93@gmail.com>
2025-05-27 11:57:03 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
 variable "existing_sg_id" {
   description = "ID of the existing security group to attach to the instance"
   type        = string
-  default     = "sg-0f43371e2b71fca11"
+  default     = "sg-02d00dfb94a29aa84"
 }
 
 variable "key_name" {
   description = "Name of the EC2 key pair to use"
   type        = string
-  default     = "886436941748_Mum_May-25"
+  default     = "AWS_KP_Value"
 }
 
